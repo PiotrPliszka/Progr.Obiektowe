62147e507e1bc5a862fdd3680dc4175ae4d1d529
PiotrPliszka <181106@student.uwm.edu.pl>
2025-12-20 21:33:16 +0100

    Dodanie Lekcji 9

--- Lekcja 9/src/Main.java
+++ Lekcja 9/src/Main.java
@@ -1,6 +1,8 @@
 import  zad2.*;
+import zad3.Triple;
 
 void main(String[] args) {
+//    ZAD2
     Counter<Integer> c = new Counter<>();
     c.add(1);
     c.add(2);
@@ -10,4 +12,14 @@ void main(String[] args) {
     c.add(6);
 
     System.out.println(c.getCount());
+
+//    ZAD3
+    Triple<String, Integer, Double> student = new Triple<>("Adam", 21, 4.5);
+
+    System.out.println("Student: " + student.getFirst());
+    System.out.println("Wiek: " + student.getSecond());
+    System.out.println("Średnia: " + student.getThird());
+
+//    ZAD4
+
 }
